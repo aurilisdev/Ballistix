adecf9f8280a9bd2fd59f1e4d2f60a088d88d062
aurilisdev <aurilisdev@gmail.com>
2021-02-09 00:09:13 +0100

    Set shrapnel damage to be absolute

--- src/main/java/ballistix/api/damage/DamageSourceShrapnel.java
+++ src/main/java/ballistix/api/damage/DamageSourceShrapnel.java
@@ -3,5 +3,5 @@ package ballistix.api.damage;
 import net.minecraft.util.DamageSource;
 
 public class DamageSourceShrapnel {
-	public static DamageSource INSTANCE = new DamageSource("shrapnel");
+	public static DamageSource INSTANCE = new DamageSource("shrapnel").setDamageIsAbsolute();
 }

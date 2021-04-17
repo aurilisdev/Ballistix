0a25d4d2b363e419095c393fe19e31512719f31e
aurilisdev <aurilisdev@gmail.com>
2021-04-17 11:27:40 +0200

    Fix JEI integration and "modernize" it

--- src/main/java/ballistix/compatability/jei/BallistixJEIPlugin.java
+++ src/main/java/ballistix/compatability/jei/BallistixJEIPlugin.java
@@ -4,9 +4,6 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
 import ballistix.compatability.jei.recipecategories.psuedorecipes.BallistixPsuedoRecipes;
 import ballistix.compatability.jei.recipecategories.specificmachines.ballistix.WarheadRecipeCategory;
 import electrodynamics.prefab.tile.processing.DO2OProcessingRecipe;
@@ -18,74 +15,66 @@ import mezz.jei.api.registration.IRecipeCategoryRegistration;
 import mezz.jei.api.registration.IRecipeRegistration;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
-import net.minecraftforge.fml.ModList;
 
 @JeiPlugin
-public class BallistixJEIPlugin implements IModPlugin{
-	
-	//public static final boolean isBallistixLoaded = ModList.get().isLoaded("ballistix");
-	
-	private static final Logger logger = LogManager.getLogger(ballistix.References.ID);
-	
-	@Override
-	public ResourceLocation getPluginUid() {
-		return new ResourceLocation(electrodynamics.api.References.ID, "balx_jei_plugin");
-	}
-	
-	@Override
-	public void registerRecipeCatalysts(IRecipeCatalystRegistration registration)
-	{
-		//Warhead Template
-		registration.addRecipeCatalyst(new ItemStack(ballistix.DeferredRegisters.ITEM_MISSILELONGRANGE.get()), 
-				WarheadRecipeCategory.UID);
-	}
-	
-	@Override
-	public void registerRecipes(IRecipeRegistration registration) 
-	{
-		BallistixPsuedoRecipes.addBallistixRecipes();
-		
-		//Warhead Template
-		Set<DO2OProcessingRecipe> warheadTemplateRecipes = new HashSet<>(BallistixPsuedoRecipes.WARHEAD_RECIPES);
-		registration.addRecipes(warheadTemplateRecipes, WarheadRecipeCategory.UID);
-		
-		ballistixInfoTabs(registration);
-			
-	}
-	
-	@Override
-	public void registerCategories(IRecipeCategoryRegistration registration) 
-	{	
-		//Warhead Template
-		registration.addRecipeCategories(new WarheadRecipeCategory(registration.getJeiHelpers().getGuiHelper()));
-	}
-	
-	private void ballistixInfoTabs(IRecipeRegistration registration) {
-		/* Items currently with tabs:
+public class BallistixJEIPlugin implements IModPlugin {
 
-		  Close Range Missile 
-		  Medium Range Missile 
-          Long Range Missile 
-          Missile Silo
-		
-		*/
-		ArrayList<ItemStack> ballistixInfoItems = BallistixPsuedoRecipes.BALLISTIX_ITEMS.get(1);
-		String temp;
-		
-		for(ItemStack itemStack: ballistixInfoItems) {
-			temp = itemStack.getItem().toString();
-			registration.addIngredientInfo(itemStack, VanillaTypes.ITEM, "info.jei.item." + temp);
-			//logger.info("Item name: " + temp);
-		}
-		
-		ballistixInfoItems = BallistixPsuedoRecipes.BALLISTIX_ITEMS.get(2);
-		
-		for(ItemStack itemStack: ballistixInfoItems) {
-			temp = itemStack.getItem().toString();
-			registration.addIngredientInfo(itemStack, VanillaTypes.ITEM, "info.jei.item." + temp);
-			//logger.info("Item name: " + temp);
-		}
-		
+    // public static final boolean isBallistixLoaded =
+    // ModList.get().isLoaded("ballistix");
+
+    @Override
+    public ResourceLocation getPluginUid() {
+	return new ResourceLocation(electrodynamics.api.References.ID, "balx_jei_plugin");
+    }
+
+    @Override
+    public void registerRecipeCatalysts(IRecipeCatalystRegistration registration) {
+	// Warhead Template
+	registration.addRecipeCatalyst(new ItemStack(ballistix.DeferredRegisters.ITEM_MISSILELONGRANGE.get()), WarheadRecipeCategory.UID);
+    }
+
+    @Override
+    public void registerRecipes(IRecipeRegistration registration) {
+	BallistixPsuedoRecipes.addBallistixRecipes();
+
+	// Warhead Template
+	Set<DO2OProcessingRecipe> warheadTemplateRecipes = new HashSet<>(BallistixPsuedoRecipes.WARHEAD_RECIPES);
+	registration.addRecipes(warheadTemplateRecipes, WarheadRecipeCategory.UID);
+
+	ballistixInfoTabs(registration);
+
+    }
+
+    @Override
+    public void registerCategories(IRecipeCategoryRegistration registration) {
+	// Warhead Template
+	registration.addRecipeCategories(new WarheadRecipeCategory(registration.getJeiHelpers().getGuiHelper()));
+    }
+
+    private static void ballistixInfoTabs(IRecipeRegistration registration) {
+	/*
+	 * Items currently with tabs:
+	 * 
+	 * Close Range Missile Medium Range Missile Long Range Missile Missile Silo
+	 * 
+	 */
+	ArrayList<ItemStack> ballistixInfoItems = BallistixPsuedoRecipes.BALLISTIX_ITEMS.get(1);
+	String temp;
+
+	for (ItemStack itemStack : ballistixInfoItems) {
+	    temp = itemStack.getItem().toString();
+	    registration.addIngredientInfo(itemStack, VanillaTypes.ITEM, "info.jei.item." + temp);
+	    // logger.info("Item name: " + temp);
 	}
+
+	ballistixInfoItems = BallistixPsuedoRecipes.BALLISTIX_ITEMS.get(2);
+
+	for (ItemStack itemStack : ballistixInfoItems) {
+	    temp = itemStack.getItem().toString();
+	    registration.addIngredientInfo(itemStack, VanillaTypes.ITEM, "info.jei.item." + temp);
+	    // logger.info("Item name: " + temp);
+	}
+
+    }
 
 }

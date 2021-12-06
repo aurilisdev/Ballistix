d1e4165ae4fafec181f399f574fe9693a9ace3f3
skip999 <skiprocks999@gmail.com>
2021-12-06 15:56:54 -0600

    re-added JEI support

---
+++ src/main/java/ballistix/compatability/jei/recipecategories/psuedo/specificmachines/WarheadRecipeCategory.java
@@ -0,0 +1,139 @@
+package ballistix.compatability.jei.recipecategories.psuedo.specificmachines;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
+import com.mojang.blaze3d.vertex.PoseStack;
+
+import ballistix.References;
+import ballistix.compatability.jei.util.psuedorecipes.BallistixPsuedoRecipes;
+import electrodynamics.compatability.jei.recipecategories.psuedo.PsuedoItem2ItemRecipe;
+import mezz.jei.api.constants.VanillaTypes;
+import mezz.jei.api.gui.IRecipeLayout;
+import mezz.jei.api.gui.drawable.IDrawable;
+import mezz.jei.api.gui.drawable.IDrawableAnimated;
+import mezz.jei.api.gui.ingredient.IGuiItemStackGroup;
+import mezz.jei.api.helpers.IGuiHelper;
+import mezz.jei.api.ingredients.IIngredients;
+import mezz.jei.api.recipe.category.IRecipeCategory;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.Font;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.TranslatableComponent;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.ItemStack;
+
+public class WarheadRecipeCategory implements IRecipeCategory<PsuedoItem2ItemRecipe> {
+
+    // JEI Window Parameters
+    private static final int INPUT_1_SLOT = 0;
+    private static final int INPUT_2_SLOT = 1;
+
+    private static int[] GUI_BACKGROUND = { 0, 0, 132, 58 };
+    private static int[] PROCESSING_ARROW_COORDS = { 0, 0, 10, 10 };
+
+    private static int[] INPUT_1_OFFSET = { 89, 7 };
+    private static int[] INPUT_2_OFFSET = { 89, 33 };
+
+    private static int[] PROCESSING_ARROW_OFFSET = { 0, 0 };
+
+    private static int ANIM_TIME = 50;
+    private static int DESC_Y_HEIGHT = 48;
+
+    private static String MOD_ID = References.ID;
+    private static String RECIPE_GROUP = "warhead_template";
+    private static String GUI_TEXTURE = "textures/gui/jei/warhead_template_gui.png";
+
+    public static ItemStack INPUT_MACHINE = new ItemStack(ballistix.DeferredRegisters.blockMissileSilo);
+
+    private IDrawable BACKGROUND;
+    private IDrawable ICON;
+
+    private static IDrawableAnimated.StartDirection START_DIRECTION = IDrawableAnimated.StartDirection.LEFT;
+    private LoadingCache<Integer, IDrawableAnimated> cachedArrows;
+
+    public static ResourceLocation UID = new ResourceLocation(MOD_ID, RECIPE_GROUP);
+
+    public WarheadRecipeCategory(IGuiHelper guiHelper) {
+
+	ICON = guiHelper.createDrawableIngredient(INPUT_MACHINE);
+	BACKGROUND = guiHelper.createDrawable(new ResourceLocation(MOD_ID, GUI_TEXTURE), GUI_BACKGROUND[0], GUI_BACKGROUND[1], GUI_BACKGROUND[2],
+		GUI_BACKGROUND[3]);
+
+	cachedArrows = CacheBuilder.newBuilder().maximumSize(25).build(new CacheLoader<Integer, IDrawableAnimated>() {
+	    @Override
+	    public IDrawableAnimated load(Integer cookTime) {
+		return guiHelper.drawableBuilder(new ResourceLocation(MOD_ID, GUI_TEXTURE), PROCESSING_ARROW_COORDS[0], PROCESSING_ARROW_COORDS[1],
+			PROCESSING_ARROW_COORDS[2], PROCESSING_ARROW_COORDS[3]).buildAnimated(cookTime, START_DIRECTION, false);
+	    }
+	});
+
+    }
+
+    @Override
+    public ResourceLocation getUid() {
+	return UID;
+    }
+
+    @Override
+    public Class<? extends PsuedoItem2ItemRecipe> getRecipeClass() {
+	return PsuedoItem2ItemRecipe.class;
+    }
+
+    @Override
+    public Component getTitle() {
+	return new TranslatableComponent("gui.jei.category." + RECIPE_GROUP);
+    }
+
+    @Override
+    public IDrawable getBackground() {
+	return BACKGROUND;
+    }
+
+    @Override
+    public IDrawable getIcon() {
+	return ICON;
+    }
+
+    @Override
+    public void setIngredients(PsuedoItem2ItemRecipe recipe, IIngredients ingredients) {
+	List<List<ItemStack>> inputSlots = new ArrayList<>();
+	inputSlots.add(BallistixPsuedoRecipes.BALLISTIX_ITEMS.get(0));
+	inputSlots.add(BallistixPsuedoRecipes.BALLISTIX_ITEMS.get(1));
+	ingredients.setInputLists(VanillaTypes.ITEM, inputSlots);
+    }
+
+    @Override
+    public void setRecipe(IRecipeLayout recipeLayout, PsuedoItem2ItemRecipe recipe, IIngredients ingredients) {
+	IGuiItemStackGroup guiItemStacks = recipeLayout.getItemStacks();
+
+	guiItemStacks.init(INPUT_2_SLOT, true, INPUT_1_OFFSET[0], INPUT_1_OFFSET[1]);
+	guiItemStacks.init(INPUT_1_SLOT, true, INPUT_2_OFFSET[0], INPUT_2_OFFSET[1]);
+
+	guiItemStacks.set(ingredients);
+    }
+
+    @Override
+    public void draw(PsuedoItem2ItemRecipe recipe, PoseStack matrixStack, double mouseX, double mouseY) {
+	IDrawableAnimated arrow = cachedArrows.getUnchecked(ANIM_TIME);
+	arrow.draw(matrixStack, PROCESSING_ARROW_OFFSET[0], PROCESSING_ARROW_OFFSET[1]);
+
+	int animTimeSeconds = ANIM_TIME / 20;
+
+	TranslatableComponent missileString = new TranslatableComponent("gui.jei.category." + RECIPE_GROUP + ".info.missile", animTimeSeconds);
+	TranslatableComponent explosiveString = new TranslatableComponent("gui.jei.category." + RECIPE_GROUP + ".info.explosive", animTimeSeconds);
+
+	Minecraft minecraft = Minecraft.getInstance();
+	Font fontRenderer = minecraft.font;
+
+	int missileWidth = fontRenderer.width(missileString);
+	int explosiveWidth = fontRenderer.width(explosiveString);
+
+	fontRenderer.draw(matrixStack, missileString, GUI_BACKGROUND[2] - missileWidth - 46f, DESC_Y_HEIGHT - 37f, 0xFF333333);
+	fontRenderer.draw(matrixStack, explosiveString, GUI_BACKGROUND[2] - explosiveWidth - 46f, DESC_Y_HEIGHT - 10f, 0xFF333333);
+    }
+
+}
---
+++ src/main/java/ballistix/compatability/jei/util/psuedorecipes/BallistixPsuedoRecipes.java
@@ -0,0 +1,56 @@
+package ballistix.compatability.jei.util.psuedorecipes;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import ballistix.common.block.SubtypeBlast;
+import electrodynamics.compatability.jei.recipecategories.psuedo.PsuedoItem2ItemRecipe;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+
+public class BallistixPsuedoRecipes {
+
+    public static ArrayList<ArrayList<ItemStack>> BALLISTIX_ITEMS = new ArrayList<>();
+
+    public static ArrayList<PsuedoItem2ItemRecipe> WARHEAD_RECIPES = new ArrayList<>();
+
+    public static void addBallistixRecipes() {
+
+	addBallistixItems();
+
+	WARHEAD_RECIPES.add(new PsuedoItem2ItemRecipe(Arrays.asList(new ItemStack[] { BALLISTIX_ITEMS.get(0).get(0), BALLISTIX_ITEMS.get(1).get(0) }),
+		BALLISTIX_ITEMS.get(1).get(0)));
+
+    }
+
+    private static void addBallistixItems() {
+
+	/* EXPLOSIVES */
+	SubtypeBlast[] explosives = SubtypeBlast.values();
+	int explosivesLength = explosives.length;
+	Item[] explosiveItems = new Item[explosivesLength];
+	for (int i = 0; i < explosivesLength; i++) {
+	    explosiveItems[i] = ballistix.DeferredRegisters.SUBTYPEBLOCK_MAPPINGS.get(explosives[i]).asItem();
+	}
+	BALLISTIX_ITEMS.add(formItemStacks(explosiveItems, 1));
+
+	/* MISSILES */
+	Item[] missiles = { ballistix.DeferredRegisters.ITEM_MISSILECLOSERANGE.get(), ballistix.DeferredRegisters.ITEM_MISSILEMEDIUMRANGE.get(),
+		ballistix.DeferredRegisters.ITEM_MISSILELONGRANGE.get() };
+	BALLISTIX_ITEMS.add(formItemStacks(missiles, 1));
+
+	/* MISC */
+	Item[] misc = { ballistix.DeferredRegisters.blockMissileSilo.asItem(), ballistix.DeferredRegisters.ITEM_ROCKETLAUNCHER.get() };
+	BALLISTIX_ITEMS.add(formItemStacks(misc, 1));
+    }
+
+    private static ArrayList<ItemStack> formItemStacks(Item[] items, int countPerItemStack) {
+	ArrayList<ItemStack> inputItems = new ArrayList<>();
+
+	for (int i = 0; i < items.length; i++) {
+	    inputItems.add(new ItemStack(items[i]));
+	    inputItems.get(i).setCount(countPerItemStack);
+	}
+	return inputItems;
+    }
+}

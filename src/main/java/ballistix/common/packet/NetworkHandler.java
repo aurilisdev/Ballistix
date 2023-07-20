ffb67399899929a37d10a644f75a3f37bca903a4
aurilisdev <aurilisdev@gmail.com>
2023-07-20 21:22:42 +0200

    Some cleanup

--- src/main/java/ballistix/common/packet/NetworkHandler.java
+++ src/main/java/ballistix/common/packet/NetworkHandler.java
@@ -8,7 +8,7 @@ import net.minecraftforge.network.simple.SimpleChannel;
 public class NetworkHandler {
 	private static final String PROTOCOL_VERSION = "1";
 	public static final SimpleChannel CHANNEL = NetworkRegistry.newSimpleChannel(new ResourceLocation(References.ID, "main"), () -> PROTOCOL_VERSION, PROTOCOL_VERSION::equals, PROTOCOL_VERSION::equals);
-	private static int disc = 0;
+//	private static int disc = 0;
 
 	public static void init() {
 

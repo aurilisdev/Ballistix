8776cba77f3fdf33f029716c4bdf916f6b90a4d8
skip999 <skiprocks999@gmail.com>
2023-09-05 17:23:54 -0500

    cleanup

--- src/main/java/ballistix/registers/BallistixCreativeTabs.java
+++ src/main/java/ballistix/registers/BallistixCreativeTabs.java
@@ -10,9 +10,9 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.RegistryObject;
 
 public class BallistixCreativeTabs {
-	
-public static final DeferredRegister<CreativeModeTab> CREATIVE_TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, References.ID);
-	
+
+	public static final DeferredRegister<CreativeModeTab> CREATIVE_TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, References.ID);
+
 	public static final RegistryObject<CreativeModeTab> MAIN = CREATIVE_TABS.register("main", () -> CreativeModeTab.builder().title(BallistixTextUtils.creativeTab("main")).icon(() -> new ItemStack(BallistixBlocks.getBlock(SubtypeBlast.antimatter))).build());
 
 }

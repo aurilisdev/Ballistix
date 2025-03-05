304ba6ef63ed25848f65384d1bafcba6835eff73
aurilisdev <aurilisdev@gmail.com>
2025-03-05 16:05:29 +0100

    Remove platform electricity (wasnt used)

--- src/main/java/ballistix/common/tile/TileLauncherPlatformT1.java
+++ src/main/java/ballistix/common/tile/TileLauncherPlatformT1.java
@@ -1,5 +1,7 @@
 package ballistix.common.tile;
 
+import org.jetbrains.annotations.Nullable;
+
 import ballistix.api.missile.MissileManager;
 import ballistix.api.missile.virtual.VirtualMissile;
 import ballistix.api.silo.ILauncherControlPanel;
@@ -10,7 +12,6 @@ import ballistix.common.inventory.container.ContainerLauncherPlatformT1;
 import ballistix.common.inventory.container.ContainerLauncherPlatformT2;
 import ballistix.common.inventory.container.ContainerLauncherPlatformT3;
 import ballistix.common.item.ItemMissile;
-import ballistix.common.settings.Constants;
 import ballistix.registers.BallistixSounds;
 import ballistix.registers.BallistixTiles;
 import electrodynamics.api.multiblock.subnodebased.parent.IMultiblockParentBlock;
@@ -22,7 +23,6 @@ import electrodynamics.prefab.properties.PropertyTypes;
 import electrodynamics.prefab.tile.GenericTile;
 import electrodynamics.prefab.tile.components.IComponentType;
 import electrodynamics.prefab.tile.components.type.ComponentContainerProvider;
-import electrodynamics.prefab.tile.components.type.ComponentElectrodynamic;
 import electrodynamics.prefab.tile.components.type.ComponentInventory;
 import electrodynamics.prefab.tile.components.type.ComponentInventory.InventoryBuilder;
 import electrodynamics.prefab.tile.components.type.ComponentPacketHandler;
@@ -31,11 +31,17 @@ import electrodynamics.prefab.utilities.BlockEntityUtils;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.sounds.SoundSource;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.ItemInteractionResult;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.Vec3;
+import net.neoforged.neoforge.items.IItemHandler;
 
 public class TileLauncherPlatformT1 extends GenericTile implements ILauncherPlatform, IMultiblockParentTile {
 
@@ -52,7 +58,6 @@ public class TileLauncherPlatformT1 extends GenericTile implements ILauncherPlat
 		super(type, pos, state);
 		int tier = getTier();
 		addComponent(new ComponentTickable(this));
-		addComponent(new ComponentElectrodynamic(this, false, true).voltage(120 * tier).maxJoules(Constants.MISSILESILO_USAGE * 20 * tier).setInputDirections(BlockEntityUtils.MachineDirection.values()));
 		addComponent(new ComponentInventory(this, InventoryBuilder.newInv().inputs(2)).setDirectionsBySlot(0, BlockEntityUtils.MachineDirection.values()).setDirectionsBySlot(1, BlockEntityUtils.MachineDirection.values()).valid(this::isItemValidForSlot));
 		addComponent(new ComponentPacketHandler(this));
 		if (tier == 1) {
@@ -159,6 +164,21 @@ public class TileLauncherPlatformT1 extends GenericTile implements ILauncherPlat
 	@Override
 	public void onSubnodeDestroyed(TileMultiSubnode tileMultiSubnode) {
 		level.destroyBlock(worldPosition, true);
+	}
+
+	@Override
+	public ItemInteractionResult onSubnodeUseWithItem(ItemStack used, Player player, InteractionHand hand, BlockHitResult hit, TileMultiSubnode subnode) {
+		return useWithItem(used, player, hand, hit);
+	}
+
+	@Override
+	public InteractionResult onSubnodeUseWithoutItem(Player player, BlockHitResult hit, TileMultiSubnode subnode) {
+		return useWithoutItem(player, hit);
+	}
+
+	@Override
+	public @Nullable IItemHandler getSubnodeItemHandlerCapability(TileMultiSubnode subnode, @Nullable Direction side) {
+		return getItemHandlerCapability(side);
 	}
 
 	@Override

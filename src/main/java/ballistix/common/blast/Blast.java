98ef3504d334ca4faa07b78b0e70e012e708c4ee
skip999 <skiprocks999@gmail.com>
2025-03-18 17:38:12 -0500

    fix client-side issue on server

--- src/main/java/ballistix/common/blast/Blast.java
+++ src/main/java/ballistix/common/blast/Blast.java
@@ -46,20 +46,20 @@ public abstract class Blast {
     public boolean shouldRenderCustomClient;
 
     protected Blast(Level world, BlockPos position) {
-	this.world = world;
-	this.position = position;
-	griefPreventionMethod = getGriefPreventionMethod();
+        this.world = world;
+        this.position = position;
+        griefPreventionMethod = getGriefPreventionMethod();
     }
 
     public static GriefPreventionMethod getGriefPreventionMethod() {
-	if (ModList.get().isLoaded(References.GRIEF_DEFENDER_ID)) {
-	    return GriefPreventionMethod.GRIEF_DEFENDER;
-	}
-	return GriefPreventionMethod.NONE;
+        if (ModList.get().isLoaded(References.GRIEF_DEFENDER_ID)) {
+            return GriefPreventionMethod.GRIEF_DEFENDER;
+        }
+        return GriefPreventionMethod.NONE;
     }
 
     public boolean isInstantaneous() {
-	return true;
+        return true;
     }
 
     public abstract SubtypeBlast getBlastType();
@@ -68,10 +68,10 @@ public abstract class Blast {
     }
 
     public boolean doExplode(int callCount) {
-	if (world.isClientSide && (shouldRenderCustomClient || this.isInstantaneous()) && this instanceof IHasCustomRender renderer) {
-	    renderer.produceParticles();
-	}
-	return false;
+        if (world.isClientSide && (shouldRenderCustomClient || this.isInstantaneous()) && this instanceof IHasCustomRender renderer) {
+            renderer.produceParticles();
+        }
+        return false;
     }
 
     public void doPostExplode() {
@@ -79,149 +79,140 @@ public abstract class Blast {
 
     @Deprecated(since = "Should not be called externally!", forRemoval = false)
     public final void preExplode() {
-	PreBlastEvent evt = new PreBlastEvent(world, this);
-	NeoForge.EVENT_BUS.post(evt);
+        PreBlastEvent evt = new PreBlastEvent(world, this);
+        NeoForge.EVENT_BUS.post(evt);
 
-	if (!evt.isCanceled()) {
-	    doPreExplode();
-	}
+        if (!evt.isCanceled()) {
+            doPreExplode();
+        }
     }
 
     @Deprecated(since = "Should not be called externally!", forRemoval = false)
     public final boolean explode(int callcount) {
-	BlastEvent evt = new BlastEvent(world, this);
-	NeoForge.EVENT_BUS.post(evt);
-	if (!evt.isCanceled()) {
-	    return doExplode(callcount);
-	}
-	return true;
+        BlastEvent evt = new BlastEvent(world, this);
+        NeoForge.EVENT_BUS.post(evt);
+        if (!evt.isCanceled()) {
+            return doExplode(callcount);
+        }
+        return true;
     }
 
     @Deprecated(since = "Should not be called externally!", forRemoval = false)
     public final void postExplode() {
-	PostBlastEvent evt = new PostBlastEvent(world, this);
-	NeoForge.EVENT_BUS.post(evt);
+        PostBlastEvent evt = new PostBlastEvent(world, this);
+        NeoForge.EVENT_BUS.post(evt);
 
-	if (!evt.isCanceled()) {
-	    doPostExplode();
-	}
+        if (!evt.isCanceled()) {
+            doPostExplode();
+        }
     }
 
     public EntityBlast performExplosion() {
-	ConstructBlastEvent evt = new ConstructBlastEvent(world, this);
-	NeoForge.EVENT_BUS.post(evt);
-	Explosion explosion = new Explosion(world, null, null, null, position.getX(), position.getY(), position.getZ(),
-		3, true, BlockInteraction.DESTROY, ParticleTypes.EXPLOSION, ParticleTypes.EXPLOSION_EMITTER,
-		SoundEvents.GENERIC_EXPLODE);
-	if (!EventHooks.onExplosionStart(world, explosion) && !evt.isCanceled()) {
-	    if (isInstantaneous() && !(this instanceof IHasCustomRender)) {
-		doPreExplode();
-		doExplode(0);
-		doPostExplode();
-	    } else if (!world.isClientSide) {
-		EntityBlast entity = new EntityBlast(world);
-		entity.setPos(position.getX() + 0.5, position.getY() + 0.5, position.getZ() + 0.5);
-		entity.setBlastType(getBlastType());
-		world.addFreshEntity(entity);
-		return entity;
-	    }
-	}
-	return null;
+        ConstructBlastEvent evt = new ConstructBlastEvent(world, this);
+        NeoForge.EVENT_BUS.post(evt);
+        Explosion explosion = new Explosion(world, null, null, null, position.getX(), position.getY(), position.getZ(), 3, true, BlockInteraction.DESTROY, ParticleTypes.EXPLOSION, ParticleTypes.EXPLOSION_EMITTER, SoundEvents.GENERIC_EXPLODE);
+        if (!EventHooks.onExplosionStart(world, explosion) && !evt.isCanceled()) {
+            if (isInstantaneous() && !(this instanceof IHasCustomRender)) {
+                doPreExplode();
+                doExplode(0);
+                doPostExplode();
+            } else if (!world.isClientSide) {
+                EntityBlast entity = new EntityBlast(world);
+                entity.setPos(position.getX() + 0.5, position.getY() + 0.5, position.getZ() + 0.5);
+                entity.setBlastType(getBlastType());
+                world.addFreshEntity(entity);
+                return entity;
+            }
+        }
+        return null;
     }
 
     protected void attackEntities(float size, Explosion explosion) {
-	this.attackEntities(size, true, explosion);
+        this.attackEntities(size, true, explosion);
     }
 
     protected void attackEntities(float size, boolean useRaytrace, Explosion explosion) {
-	Map<Player, Vec3> playerKnockbackMap = Maps.newHashMap();
-	float doubleSize = size * 2.0F;
-	int x0 = Mth.floor(position.getX() - (double) doubleSize - 1.0D);
-	int x1 = Mth.floor(position.getX() + (double) doubleSize + 1.0D);
-	int y0 = Mth.floor(position.getY() - (double) doubleSize - 1.0D);
-	int y1 = Mth.floor(position.getY() + (double) doubleSize + 1.0D);
-	int z0 = Mth.floor(position.getZ() - (double) doubleSize - 1.0D);
-	int z1 = Mth.floor(position.getZ() + (double) doubleSize + 1.0D);
+        Map<Player, Vec3> playerKnockbackMap = Maps.newHashMap();
+        float doubleSize = size * 2.0F;
+        int x0 = Mth.floor(position.getX() - (double) doubleSize - 1.0D);
+        int x1 = Mth.floor(position.getX() + (double) doubleSize + 1.0D);
+        int y0 = Mth.floor(position.getY() - (double) doubleSize - 1.0D);
+        int y1 = Mth.floor(position.getY() + (double) doubleSize + 1.0D);
+        int z0 = Mth.floor(position.getZ() - (double) doubleSize - 1.0D);
+        int z1 = Mth.floor(position.getZ() + (double) doubleSize + 1.0D);
 
-	List<Entity> entities = world.getEntities(null, new AABB(x0, y0, z0, x1, y1, z1));
+        List<Entity> entities = world.getEntities(null, new AABB(x0, y0, z0, x1, y1, z1));
 
-	Vec3 posVector = new Vec3(position.getX(), position.getY(), position.getZ());
+        Vec3 posVector = new Vec3(position.getX(), position.getY(), position.getZ());
 
-	for (Entity entity : entities) {
+        for (Entity entity : entities) {
 
-	    boolean ignoreExplosion = entity.ignoreExplosion(explosion);
+            boolean ignoreExplosion = entity.ignoreExplosion(explosion);
 
-	    switch (griefPreventionMethod) {
-	    case NONE:
-		if (ignoreExplosion) {
-		    continue;
-		}
-		break;
-	    case GRIEF_DEFENDER:
-		if (!GriefDefenderHandler.shouldEntityBeHarmed(entity) && ignoreExplosion) {
-		    continue;
-		}
-		break;
-	    case SABER_FACTIONS:
+            switch (griefPreventionMethod) {
+                case NONE:
+                    if (ignoreExplosion) {
+                        continue;
+                    }
+                    break;
+                case GRIEF_DEFENDER:
+                    if (!GriefDefenderHandler.shouldEntityBeHarmed(entity) && ignoreExplosion) {
+                        continue;
+                    }
+                    break;
+                case SABER_FACTIONS:
 
-		break;
+                    break;
 
-	    }
+            }
 
-	    double normalizedDiameter = Mth.sqrt((float) entity.distanceToSqr(posVector)) / doubleSize;
-	    if (normalizedDiameter > 1.0D) {
-		continue;
-	    }
-	    double deltaX = entity.getX() - position.getX();
-	    double deltaY = (entity instanceof PrimedTnt ? entity.getY() : entity.getEyeY()) - position.getY();
-	    double deltaZ = entity.getZ() - position.getZ();
-	    double deltaDistance = Mth.sqrt((float) (deltaX * deltaX + deltaY * deltaY + deltaZ * deltaZ));
+            double normalizedDiameter = Mth.sqrt((float) entity.distanceToSqr(posVector)) / doubleSize;
+            if (normalizedDiameter > 1.0D) {
+                continue;
+            }
+            double deltaX = entity.getX() - position.getX();
+            double deltaY = (entity instanceof PrimedTnt ? entity.getY() : entity.getEyeY()) - position.getY();
+            double deltaZ = entity.getZ() - position.getZ();
+            double deltaDistance = Mth.sqrt((float) (deltaX * deltaX + deltaY * deltaY + deltaZ * deltaZ));
 
-	    if (deltaDistance == 0.0D) {
-		continue;
-	    }
+            if (deltaDistance == 0.0D) {
+                continue;
+            }
 
-	    deltaX = deltaX / deltaDistance;
-	    deltaY = deltaY / deltaDistance;
-	    deltaZ = deltaZ / deltaDistance;
+            deltaX = deltaX / deltaDistance;
+            deltaY = deltaY / deltaDistance;
+            deltaZ = deltaZ / deltaDistance;
 
-	    double seenAmount = useRaytrace ? Explosion.getSeenPercent(posVector, entity) : 1;
+            double seenAmount = useRaytrace ? Explosion.getSeenPercent(posVector, entity) : 1;
 
-	    double damageAmount = (1.0D - normalizedDiameter) * seenAmount;
+            double damageAmount = (1.0D - normalizedDiameter) * seenAmount;
 
-	    entity.hurt(entity.damageSources().explosion(null, null),
-		    (int) ((damageAmount * damageAmount + damageAmount) / 2.0D * 7.0D * doubleSize + 1.0D));
+            entity.hurt(entity.damageSources().explosion(null, null), (int) ((damageAmount * damageAmount + damageAmount) / 2.0D * 7.0D * doubleSize + 1.0D));
 
-	    double actualDamange = damageAmount;
+            double actualDamange = damageAmount;
 
-	    if (entity instanceof LivingEntity le) {
-		double damage = damageAmount;
-		int i = EnchantmentHelper
-			.getEnchantmentLevel(world.registryAccess().holderOrThrow(Enchantments.BLAST_PROTECTION), le);
-		if (i > 0) {
-		    damage *= Mth.clamp(1.0D - i * 0.15D, 0.0D, 1.0D);
-		}
+            if (entity instanceof LivingEntity le) {
+                double damage = damageAmount;
+                int i = EnchantmentHelper.getEnchantmentLevel(world.registryAccess().holderOrThrow(Enchantments.BLAST_PROTECTION), le);
+                if (i > 0) {
+                    damage *= Mth.clamp(1.0D - i * 0.15D, 0.0D, 1.0D);
+                }
 
-		actualDamange = damage;
-	    }
+                actualDamange = damage;
+            }
 
-	    entity.setDeltaMovement(entity.getDeltaMovement().add(deltaX * actualDamange, deltaY * actualDamange,
-		    deltaZ * actualDamange));
-	    if (entity instanceof Player playerentity) {
-		if (!playerentity.isSpectator()
-			&& (!playerentity.isCreative() || !playerentity.getAbilities().flying)) {
-		    playerKnockbackMap.put(playerentity,
-			    new Vec3(deltaX * damageAmount, deltaY * damageAmount, deltaZ * damageAmount));
-		}
-	    }
-	}
-	for (Entry<Player, Vec3> entry : playerKnockbackMap.entrySet()) {
-	    if (entry.getKey() instanceof ServerPlayer serverplayerentity) {
-		serverplayerentity.connection.send(new ClientboundExplodePacket(position.getX(), position.getY(),
-			position.getZ(), size, new ArrayList<>(), entry.getValue(), BlockInteraction.DESTROY,
-			ParticleTypes.EXPLOSION, ParticleTypes.EXPLOSION_EMITTER, SoundEvents.GENERIC_EXPLODE));
-	    }
-	}
+            entity.setDeltaMovement(entity.getDeltaMovement().add(deltaX * actualDamange, deltaY * actualDamange, deltaZ * actualDamange));
+            if (entity instanceof Player playerentity) {
+                if (!playerentity.isSpectator() && (!playerentity.isCreative() || !playerentity.getAbilities().flying)) {
+                    playerKnockbackMap.put(playerentity, new Vec3(deltaX * damageAmount, deltaY * damageAmount, deltaZ * damageAmount));
+                }
+            }
+        }
+        for (Entry<Player, Vec3> entry : playerKnockbackMap.entrySet()) {
+            if (entry.getKey() instanceof ServerPlayer serverplayerentity) {
+                serverplayerentity.connection.send(new ClientboundExplodePacket(position.getX(), position.getY(), position.getZ(), size, new ArrayList<>(), entry.getValue(), BlockInteraction.DESTROY, ParticleTypes.EXPLOSION, ParticleTypes.EXPLOSION_EMITTER, SoundEvents.GENERIC_EXPLODE));
+            }
+        }
     }
 
     // public FakePlayer getFakePlayer(ServerLevel level) {
@@ -230,12 +221,12 @@ public abstract class Blast {
     // }
 
     public static interface BlastFactory<T extends Blast> {
-	T create(Level world, BlockPos pos);
+        T create(Level world, BlockPos pos);
     }
 
     public static enum GriefPreventionMethod {
 
-	NONE, GRIEF_DEFENDER, SABER_FACTIONS;
+        NONE, GRIEF_DEFENDER, SABER_FACTIONS;
 
     }
 }

1143410232cab32b228877e2bb1b354c0d1fd471
aurilisdev <aurilisdev@gmail.com>
2025-12-09 00:01:25 +0100

    Less but larget particles + Proper block break flags

--- src/main/java/ballistix/compatibility/nuclearscience/RadiationHandler.java
+++ src/main/java/ballistix/compatibility/nuclearscience/RadiationHandler.java
@@ -27,7 +27,7 @@ public class RadiationHandler {
             }
         } else if (state.is(BlockTags.LEAVES)) {
             world.setBlock(p, Blocks.AIR.defaultBlockState(), 3);
-            if (world.random.nextFloat() < 1 / 15.0 && world instanceof ServerLevel serverlevel) {
+            if (world.random.nextFloat() < 1 / 25.0 && world instanceof ServerLevel serverlevel) {
                 serverlevel.getChunkSource().chunkMap.getPlayers(new ChunkPos(p), false).forEach(pl -> PacketDistributor.sendToPlayer(pl, new PacketSpawnBlastParticle(p, BlastParticleSpawnType.LEAVES_BREAKING)));
 //		Minecraft.getInstance().particleEngine.createParticle(ParticleTypes.GUST, p.getX() + 0.5,
 //			p.getY() + 1.5, p.getZ() + 0.5, 0, 0, 0);

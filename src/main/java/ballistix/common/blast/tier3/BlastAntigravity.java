c32897485f122fd08c4d7bb20b4b030751a7e61e
aurilisdev <aurilisdev@gmail.com>
2026-07-29 19:09:51 +0200

    Patch 11: Fix antigravity blasts and add gravity to rocket launcheds

--- src/main/java/ballistix/common/blast/tier3/BlastAntigravity.java
+++ src/main/java/ballistix/common/blast/tier3/BlastAntigravity.java
@@ -1,61 +1,135 @@
 package ballistix.common.blast.tier3;
 
+import java.util.HashSet;
+
 import javax.annotation.Nullable;
 
-import ballistix.api.blast.AntigravedChunk;
 import ballistix.api.blast.IBlast;
-import ballistix.common.blast.util.Blast;
+import ballistix.common.blast.util.BlastLasting;
 import ballistix.common.block.subtype.SubtypeBlast;
+import ballistix.common.entity.EntityBallistixFallingBlock;
 import ballistix.common.settings.BallistixConstants;
-import ballistix.registers.BallistixCapabilities;
 import net.minecraft.core.BlockPos;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.entity.Entity;
-import net.minecraft.world.level.ChunkPos;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.AABB;
+import net.minecraft.world.phys.Vec3;
 
-public class BlastAntigravity extends Blast {
+public class BlastAntigravity extends BlastLasting {
+
     public BlastAntigravity(Level world, BlockPos position, @Nullable Entity owner, @Nullable Entity blastEntity) {
- 	super(world, position, owner, blastEntity);
-     }
+	super(world, position, owner, blastEntity);
+    }
 
     @Override
     public IBlast getBlastType() {
-        return SubtypeBlast.antigravity;
+	return SubtypeBlast.antigravity;
     }
 
     @Override
     public void doPreExplode() {
-        if(!world.isClientSide) {
-            world.playSound(null, position, SoundEvents.END_PORTAL_SPAWN, SoundSource.BLOCKS, 25, 1);
-        }
+	if (!world.isClientSide) {
+	    world.playSound(null, position, SoundEvents.END_PORTAL_SPAWN, SoundSource.BLOCKS, 25, 1);
+	}
     }
 
     @Override
     public boolean doExplode(int callCount) {
-        super.doExplode(callCount);
+	super.doExplode(callCount);
 
-        if(world.isClientSide()) {
-            return false;
-        }
+	if (world.isClientSide) {
+	    return false;
+	}
 
-        for(int i = -BallistixConstants.EXPLOSIVE_ANTIGRAVITY_CHUNKRADIUS; i < BallistixConstants.EXPLOSIVE_ANTIGRAVITY_CHUNKRADIUS; i++) {
-            for(int j = -BallistixConstants.EXPLOSIVE_ANTIGRAVITY_CHUNKRADIUS; j < BallistixConstants.EXPLOSIVE_ANTIGRAVITY_CHUNKRADIUS; j++) {
+	liftEntities();
+	launchBlock();
 
-                BlockPos pos = position.offset(i * 16, 0, j * 16);
-
-                ChunkPos chunkPos = new ChunkPos(pos);
-                
-                world.getCapability(BallistixCapabilities.ANTIGRAVED_CHUNKS).ifPresent(cap -> {
-                	cap.activeChunks.add(new AntigravedChunk(chunkPos, BallistixConstants.EXPLOSIVE_ANTIGRAVITY_CHUNKDURATION));
-                });
-
-            }
-
-        }
-
-        return true;
+	return ticksSinceBlastStart >= BallistixConstants.EXPLOSIVE_ANTIGRAVITY_CHUNKDURATION;
     }
 
+    private void liftEntities() {
+	int radius = BallistixConstants.EXPLOSIVE_ANTIGRAVITY_RADIUS;
+	double radiusSquared = radius * radius;
+
+	Vec3 center = Vec3.atCenterOf(position);
+
+	double minY = Math.max(world.getMinBuildHeight(), center.y - radius);
+
+	double maxY = Math.min(world.getMaxBuildHeight(), center.y + Math.max(radius, 100));
+
+	AABB bounds = new AABB(center.x - radius, minY, center.z - radius, center.x + radius, maxY, center.z + radius);
+	for (Entity entity : world.getEntitiesOfClass(Entity.class, bounds)) {
+	    if (!entity.isAlive()) {
+		continue;
+	    }
+
+	    Vec3 entityCenter = entity.getBoundingBox().getCenter();
+
+	    double deltaX = entityCenter.x - center.x;
+	    double deltaZ = entityCenter.z - center.z;
+
+	    if (deltaX * deltaX + deltaZ * deltaZ > radiusSquared) {
+		continue;
+	    }
+
+	    double lift = (entity instanceof LivingEntity ? LivingEntity.DEFAULT_BASE_GRAVITY : 0.04D)
+		    * BallistixConstants.EXPLOSIVE_ANTIGRAVITY_GRAVITYFACTOR;
+
+	    entity.push(0, lift, 0);
+	    entity.hurtMarked = true;
+	}
+    }
+
+    private void launchBlock() {
+	int radius = Math.max(1, (int) (BallistixConstants.EXPLOSIVE_ANTIGRAVITY_RADIUS * 0.8F));
+
+	for (int attempt = 0; attempt < 16; attempt++) {
+	    int x = world.random.nextIntBetweenInclusive(-radius, radius);
+	    int y = world.random.nextIntBetweenInclusive(-radius, radius);
+	    int z = world.random.nextIntBetweenInclusive(-radius, radius);
+
+	    if (x * x + z * z > radius * radius) {
+		continue;
+	    }
+
+	    BlockPos pos = position.offset(x, y, z);
+	    BlockState state = world.getBlockState(pos);
+
+	    if (state.isAir() || state.getMaterial().isLiquid() || state.getDestroySpeed(world, pos) < 0) {
+		continue;
+	    }
+
+	    BlockState above = world.getBlockState(pos.above());
+
+	    if (!above.isAir() && !above.getMaterial().isLiquid()) {
+		continue;
+	    }
+
+	    EntityBallistixFallingBlock block = new EntityBallistixFallingBlock(world, pos.getX() + 0.5,
+		    pos.getY() + 0.5, pos.getZ() + 0.5, state, new HashSet<>(), owner);
+
+	    block.setDeltaMovement(0, 1.0, 0);
+
+	    if (world.addFreshEntity(block)) {
+		world.setBlock(pos, state.getFluidState().createLegacyBlock(), Block.UPDATE_ALL);
+	    }
+
+	    break;
+	}
+    }
+
+    @Override
+    public boolean isDoneCalculating() {
+	return true;
+    }
+
+    @Override
+    public boolean isInstantaneous() {
+	return false;
+    }
 }
--- src/main/java/ballistix/registers/BallistixCapabilities.java
+++ src/main/java/ballistix/registers/BallistixCapabilities.java
@@ -4,7 +4,6 @@ import ballistix.api.capability.CapabilityActiveBullets;
 import ballistix.api.capability.CapabilityActiveMissiles;
 import ballistix.api.capability.CapabilityActiveRailgunRounds;
 import ballistix.api.capability.CapabilityActiveSAMs;
-import ballistix.api.capability.CapabilityAntigravedChunks;
 import ballistix.api.capability.CapabilitySiloRegistry;
 import net.minecraftforge.common.capabilities.Capability;
 import net.minecraftforge.common.capabilities.CapabilityManager;
@@ -13,32 +12,31 @@ import net.minecraftforge.common.capabilities.RegisterCapabilitiesEvent;
 
 public class BallistixCapabilities {
 
-	public static final Capability<CapabilitySiloRegistry> SILO_REGISTRY = CapabilityManager.get(new CapabilityToken<>() {
-	});
-	
-	public static final Capability<CapabilityActiveMissiles> ACTIVE_MISSILES = CapabilityManager.get(new CapabilityToken<>() {
-	});
-	
-	public static final Capability<CapabilityActiveBullets> ACTIVE_BULLETS = CapabilityManager.get(new CapabilityToken<>() {
-	});
-	
-	public static final Capability<CapabilityActiveRailgunRounds> ACTIVE_RAILGUN_ROUNDS = CapabilityManager.get(new CapabilityToken<>() {
-	});
-	
-	public static final Capability<CapabilityActiveSAMs> ACTIVE_SAMS = CapabilityManager.get(new CapabilityToken<>() {
-	});
-	
-	public static final Capability<CapabilityAntigravedChunks> ANTIGRAVED_CHUNKS = CapabilityManager.get(new CapabilityToken<>() {
-	});
+    public static final Capability<CapabilitySiloRegistry> SILO_REGISTRY = CapabilityManager
+	    .get(new CapabilityToken<>() {
+	    });
 
+    public static final Capability<CapabilityActiveMissiles> ACTIVE_MISSILES = CapabilityManager
+	    .get(new CapabilityToken<>() {
+	    });
 
-	public static void register(RegisterCapabilitiesEvent event) {
-		event.register(CapabilitySiloRegistry.class);
-		event.register(CapabilityActiveMissiles.class);
-		event.register(CapabilityActiveBullets.class);
-		event.register(CapabilityActiveRailgunRounds.class);
-		event.register(CapabilityActiveSAMs.class);
-		event.register(CapabilityAntigravedChunks.class);
-	}
+    public static final Capability<CapabilityActiveBullets> ACTIVE_BULLETS = CapabilityManager
+	    .get(new CapabilityToken<>() {
+	    });
+
+    public static final Capability<CapabilityActiveRailgunRounds> ACTIVE_RAILGUN_ROUNDS = CapabilityManager
+	    .get(new CapabilityToken<>() {
+	    });
+
+    public static final Capability<CapabilityActiveSAMs> ACTIVE_SAMS = CapabilityManager.get(new CapabilityToken<>() {
+    });
+
+    public static void register(RegisterCapabilitiesEvent event) {
+	event.register(CapabilitySiloRegistry.class);
+	event.register(CapabilityActiveMissiles.class);
+	event.register(CapabilityActiveBullets.class);
+	event.register(CapabilityActiveRailgunRounds.class);
+	event.register(CapabilityActiveSAMs.class);
+    }
 
 }
